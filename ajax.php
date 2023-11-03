c192472edfb124a826282b0240fff55b0a59e6ad
nrubioh <nrubiohoskins@gmail.com>
2023-11-03 17:26:34 -0300

    method 4 form data + no ajax.setRequestHeader(url)+no send(url)

--- ajax.php
+++ ajax.php
@@ -1,9 +1,12 @@
 <?php
+
 echo ("<pre>");
     echo "<b>GET STUFF</b>".'</br>';
     var_dump($_GET);
     echo "<b>POST STUFF</b>".'</br>';
     var_dump($_POST);
+    echo "<b>POST USER</b>".'</br>';
+    var_dump($_POST['user']);
     echo "<b>FILES STUFF</b>".'</br>';
     var_dump($_FILES);
 echo ("</pre>");    
